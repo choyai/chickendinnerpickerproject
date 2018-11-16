6e9670f0684b8d0a20d11b03fcbc132e838893be
choyai <choyaichaiyo@gmail.com>
2018-11-17 00:29:13 +0700

    -gripper and home controlled by serial

--- PIC/SWT/serialcom.c
+++ PIC/SWT/serialcom.c
@@ -1,5 +1,6 @@
 #include <24FJ48GA002.h>
 #include "BL_Support.h"
+#include "math.h"
 #use delay(internal = 8 MHz, clock = 32MHz)
 
 #PIN_SELECT U1RX = PIN_B12 // PIN_B14 //
@@ -7,23 +8,23 @@
 #use rs232(UART1, BAUD = 9600, XMIT = PIN_B13, RCV = PIN_B12)
 
 #define DEVICE_ID 2
-#define limitSw_x PIN_B8        //
-#define limitSw_y PIN_A2        //
-#define limitSw_z PIN_A4        //
-#define Motor_Bp PIN_B10        // Pin output is connected to DXI0  (PWM)
-#define Motor_Br PIN_B2         // Pin output is connected to DX02
-#define Motor_Bl PIN_B3         // Pin output is connected to DX03
-#define Motor_Ap PIN_B4         // Pin output is connected to DX03
-#define Motor_Ar PIN_A0         // Pin output is connected to DX03
-#define Motor_Al PIN_A1         // Pin output is connected to DX03
-#define Motor_Zp PIN_B14        // Pin output is connected to DX03
-#define Motor_Zr PIN_B15        // Pin output is connected to DX03
-#define Motor_Zl PIN_B9         // Pin output is connected to DX03
-#define Encode_A PIN_B7         // Pin output is connected to DX03
-#define Encode_B PIN_B6         // Pin output is connected to DX03
-#define Encode_Z PIN_B5         // Pin output is connected to DX03
-#define servo_r PIN_B0          // servo 270
-#define servo_l PIN_B1          // servo 180
+#define limitSw_x PIN_B8 //
+#define limitSw_y PIN_A2 //
+#define limitSw_z PIN_A4 //
+#define Motor_Bp PIN_B10 // Pin output is connected to DXI0  (PWM)
+#define Motor_Br PIN_B2  // Pin output is connected to DX02
+#define Motor_Bl PIN_B3  // Pin output is connected to DX03
+#define Motor_Ap PIN_B4  // Pin output is connected to DX03
+#define Motor_Ar PIN_A0  // Pin output is connected to DX03
+#define Motor_Al PIN_A1  // Pin output is connected to DX03
+#define Motor_Zp PIN_B14 // Pin output is connected to DX03
+#define Motor_Zr PIN_B15 // Pin output is connected to DX03
+#define Motor_Zl PIN_B9  // Pin output is connected to DX03
+#define Encode_A PIN_B7  // Pin output is connected to DX03
+#define Encode_B PIN_B6  // Pin output is connected to DX03
+#define Encode_Z PIN_B5  // Pin output is connected to DX03
+#define servo_r PIN_B0   // servo 270
+#define servo_l PIN_B1   // servo 180
 
 #PIN_SELECT OC1 = Motor_Bp
 #PIN_SELECT OC2 = Motor_Ap
@@ -33,13 +34,37 @@
 #PIN_SELECT INT1 = Encode_B
 #PIN_SELECT INT2 = Encode_Z
 
-
 // long count = 0;
 long count_a = 0;
 long count_b = 0;
 long count_z = 0;
-long posi = 0;
-int direction = 0;
+// long posi = 0;
+int u_a;
+int u_b;
+int u_z;
+
+int s_a = 0;
+int s_b = 0;
+int s_z = 0;
+int p_a = 0;
+int p_b = 0;
+int p_z = 0;
+
+int tolerance = 5;
+
+float K_Pz = 0.6;
+float K_Iz = 0.01;
+float K_Dz = 0.5;
+float K_Pa = 0.6;
+float K_Ia = 0.01;
+float K_Da = 0.5;
+float K_Pb = 0.6;
+float K_Ib = 0.01;
+float K_Db = 0.5;
+
+int direction_z = 0;
+int direction_a = 0;
+int direction_b = 0;
 char array[20] = {};
 char SM_id = 0;
 int getPackage = 0;
@@ -59,34 +84,44 @@ char command_ID;
 //     sprintf(stringResult, "%d.%d", intDist, dotDist);
 // }
 
-//Encoder Interrupts
+// Encoder Interrupts
 #INT_EXT0
 void INT_EXT_INPUT0(void) {
-      count_a++;
+  if (direction_a == 0) {
+    count_a++;
+  } else {
+    count_a--;
+  }
 }
 
 #INT_EXT1
 void INT_EXT_INPUT1(void) {
-      count_b++;
+  if (direction_b == 0) {
+    count_b++;
+  } else {
+    count_b--;
+  }
 }
 
 #INT_EXT2
 void INT_EXT_INPUT2(void) {
-      count_z++;
+  if (direction_z == 0) {
+    count_z++;
+  } else {
+    count_z--;
+  }
 }
 
 void Init_Interrupts() {
-	enable_interrupts( INT_EXT0 );
-    ext_int_edge( 0, L_TO_H ); // Rising Edge
-	enable_interrupts( INT_EXT1 );
-    ext_int_edge( 1, L_TO_H ); // Rising Edge
-	enable_interrupts( INT_EXT2 );
-    ext_int_edge( 2, L_TO_H ); // Rising Edge
+  enable_interrupts(INT_EXT0);
+  ext_int_edge(0, L_TO_H); // Rising Edge
+  enable_interrupts(INT_EXT1);
+  ext_int_edge(1, L_TO_H); // Rising Edge
+  enable_interrupts(INT_EXT2);
+  ext_int_edge(2, L_TO_H); // Rising Edge
 }
 
-
 //
-
 
 // Communication Routines
 
@@ -115,135 +150,170 @@ void SM_RxD(int c) {
   }
 }
 
-#INT_RDA // receive data interrupt one time per one
+#INT_RDA
 void UART1_Isr() {
   int c = getc();
   // putc(c);
   SM_RxD(c);
 }
+//
 
-//Motor
-void Motor_z(int u){
-  if (u > 100)u = 100;
-  if (u < -100)u = -100;
-  if(u>0){
-    output_bit(Motor_Zr,0);
-    output_bit(Motor_Zl,1);
-    direction = 0;
+// Motor
+void Motor_z(int u) {
+  if (u > 100)
+    u = 100;
+  if (u < -100)
+    u = -100;
+  if (u > 0) {
+    output_bit(Motor_Zr, 0);
+    output_bit(Motor_Zl, 1);
+    direction_z = 0;
     set_pwm_duty(3, (int)(2 * u));
-  }
-  else if(u<0) {
-    output_bit(Motor_Zr,1);
-    output_bit(Motor_Zl,0);
-    direction = 1;
+  } else if (u < 0) {
+    output_bit(Motor_Zr, 1);
+    output_bit(Motor_Zl, 0);
+    direction_z = 1;
     set_pwm_duty(3, (int)(2 * -u));
-  }else{
-    output_bit(Motor_Zr,1);
-    output_bit(Motor_Zl,1);
+  } else {
+    output_bit(Motor_Zr, 1);
+    output_bit(Motor_Zl, 1);
     set_pwm_duty(3, (int)(100));
     delay_ms(100);
   }
 }
 
-void Motor_A(int u){
-  if (u > 100)u = 100;
-  if (u < -100)u = -100;
-  if(u>0){
-    output_bit(Motor_Ar,1);
-    output_bit(Motor_Al,0);
-    direction = 0;
+void Motor_a(int u) {
+  if (u > 100)
+    u = 100;
+  if (u < -100)
+    u = -100;
+  if (u > 0) {
+    output_bit(Motor_Ar, 1);
+    output_bit(Motor_Al, 0);
+    direction_a = 0;
     set_pwm_duty(2, (int)(2 * u));
-  }
-  else if(u<0) {
-    output_bit(Motor_Ar,0);
-    output_bit(Motor_Al,1);
-    direction = 1;
+  } else if (u < 0) {
+    output_bit(Motor_Ar, 0);
+    output_bit(Motor_Al, 1);
+    direction_a = 1;
     set_pwm_duty(2, (int)(2 * -u));
-  }else{
-    output_bit(Motor_Ar,1);
-    output_bit(Motor_Al,1);
+  } else {
+    output_bit(Motor_Ar, 1);
+    output_bit(Motor_Al, 1);
     set_pwm_duty(2, (int)(100));
     delay_ms(100);
   }
 }
 
-void Motor_B(int u){
-  if (u > 100)u = 100;
-  if (u < -100)u = -100;
-  if(u>0){
-    output_bit(Motor_Br,1);
-    output_bit(Motor_Bl,0);
-    direction = 0;
+void Motor_b(int u) {
+  if (u > 100)
+    u = 100;
+  if (u < -100)
+    u = -100;
+  if (u > 0) {
+    output_bit(Motor_Br, 1);
+    output_bit(Motor_Bl, 0);
+    direction_b = 0;
     set_pwm_duty(1, (int)(2 * u));
-  }
-  else if(u<0) {
-    output_bit(Motor_Br,0);
-    output_bit(Motor_Bl,1);
-    direction = 1;
+  } else if (u < 0) {
+    output_bit(Motor_Br, 0);
+    output_bit(Motor_Bl, 1);
+    direction_b = 1;
     set_pwm_duty(1, (int)(2 * -u));
-  }else{
-    output_bit(Motor_Br,1);
-    output_bit(Motor_Bl,1);
+  } else {
+    output_bit(Motor_Br, 1);
+    output_bit(Motor_Bl, 1);
     set_pwm_duty(1, (int)(100));
     delay_ms(100);
   }
 }
+
+void PID(long r, long count, long s, long p, int *u, float K_P, float K_I,
+         float K_D) {
+  long e = r - count;
+  s = s + e;
+  *u = K_P * e + K_I * s + K_D * (e - p);
+  p = e;
+}
 //
+
+// Utilities
+int mergeInts(int MSB, int LSB) { return 256 * MSB + LSB; }
+//
+
 // COMMANDS//
 void setHome() {
   setup_compare(3, COMPARE_PWM | COMPARE_TIMER3);
-    setup_compare(2, COMPARE_PWM | COMPARE_TIMER3);
-	setup_compare(1, COMPARE_PWM | COMPARE_TIMER3);
-	set_pwm_duty(3,0);
-	set_pwm_duty(2,0);
-    set_pwm_duty(1,0);
-	do{
-		Motor_z(-100);
-	}while(input(limitSw_z)==1);
-	Motor_z(0);
-	do{
-		Motor_a(-100);
-		Motor_b(-100);
-	}while(input(limitSw_x)==1);
-	Motor_a(0);
-	Motor_b(0);
-	do{
-		Motor_a(100);
-		Motor_b(-100);
-	}while(input(limitSw_y)==1);
-	Motor_a(0);
-	Motor_b(0);
+  setup_compare(2, COMPARE_PWM | COMPARE_TIMER3);
+  setup_compare(1, COMPARE_PWM | COMPARE_TIMER3);
+  set_pwm_duty(3, 0);
+  set_pwm_duty(2, 0);
+  set_pwm_duty(1, 0);
+  do {
+    Motor_z(-100);
+  } while (input(limitSw_z) == 1);
+  Motor_z(0);
+  do {
+    Motor_a(-100);
+    Motor_b(-100);
+  } while (input(limitSw_x) == 1);
+  Motor_a(0);
+  Motor_b(0);
+  do {
+    Motor_a(100);
+    Motor_b(-100);
+  } while (input(limitSw_y) == 1);
+  Motor_a(0);
+  Motor_b(0);
   printf("done");
   getPackage = 0;
 }
 
 void setPosAB() {
+  long r_a = mergeInts((int)array[3], (int)array[4]);
+  printf("r_a = %d", (int)r_a);
+  long r_b = mergeInts((int)array[5], (int)array[6]);
+  printf("r_b = %d", (int)r_b);
+  while (abs(r_a - count_a) > tolerance && abs(r_b - count_b) > tolerance) {
+    PID(r_a, count_a, s_a, p_a, &u_a, K_Pa, K_Ia, K_Da);
+    PID(r_b, count_b, s_b, p_b, &u_b, K_Pb, K_Ib, K_Db);
+    Motor_a(u_a);
+    Motor_b(u_b);
+  }
+  printf("position = %d, %d\n", count_a, count_b);
   printf("done");
   getPackage = 0;
 }
 
 void setPosZ() {
+  long r_z = mergeInts((int)array[3], (int)array[4]);
+  printf("r_z = %d", (int)r_z);
+  while (abs(r_z - count_z) > tolerance) {
+    PID(r_z, count_z, s_z, p_z, &u_z, K_Pz, K_Iz, K_Dz);
+  }
+  Motor_z(u_z);
+  printf("position = %d\n", count_z);
   printf("done");
   getPackage = 0;
 }
 
-void gripClose(){
+void gripClose() {
   setup_compare(5, COMPARE_PWM | COMPARE_TIMER2);
-  set_pwm_duty(5, 1600);
+  set_pwm_duty(5, 2600);
   delay_ms(500);
   printf("done");
   getPackage = 0;
 }
 
-void gripOpen(){
+void gripOpen() {
   setup_compare(5, COMPARE_PWM | COMPARE_TIMER2);
-  set_pwm_duty(5, 4800);
+  set_pwm_duty(5, 4000);
   delay_ms(500);
   printf("done");
   getPackage = 0;
 }
 
-void gripRotate(){
+void gripRotate() {
   printf("done");
   getPackage = 0;
 }
@@ -266,7 +336,8 @@ int sumCheck() {
 void main() {
   disable_interrupts(GLOBAL);
 
-  clear_interrupt(INT_RDA); // recommend style coding to confirm everything clear before use
+  clear_interrupt(
+      INT_RDA); // recommend style coding to confirm everything clear before use
 
   enable_interrupts(INT_RDA);
   Init_Interrupts();
@@ -274,6 +345,12 @@ void main() {
   setup_timer3(TMR_INTERNAL | TMR_DIV_BY_8, 200);
   setup_timer2(TMR_INTERNAL | TMR_DIV_BY_8, 8000);
   setup_timer1(TMR_INTERNAL | TMR_DIV_BY_8, 6666);
+  setup_compare(3, COMPARE_PWM | COMPARE_TIMER3);
+  setup_compare(2, COMPARE_PWM | COMPARE_TIMER3);
+  setup_compare(1, COMPARE_PWM | COMPARE_TIMER3);
+  set_pwm_duty(3, 0);
+  set_pwm_duty(2, 0);
+  set_pwm_duty(1, 0);
   gripOpen();
   delay_ms(1000);
   gripClose();
